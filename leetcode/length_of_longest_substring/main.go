120b756b1be7c9cf777b067539eccc68d71864f0
lowdad <javawcy@outlook.com>
2022-04-25 21:34:24 +0800

    fix: leetcode

--- leetcode/length_of_longest_substring/main.go
+++ leetcode/length_of_longest_substring/main.go
@@ -1,45 +1,31 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
 func main() {
-	fmt.Println(lengthOfLongestSubstring("abc"))
+	fmt.Println(lengthOfLongestSubstring("aabc"))
 }
 
 func lengthOfLongestSubstring(s string) int {
+	window := map[byte]int{}
+	right := -1
 	max := 0
-	if len(s) == 0 {
-		return 0
-	}
+	for left := 0; left < len(s); left++ {
+		//左边界移动一格删除前一格数据
+		if left > 0 {
+			delete(window, s[left-1])
+		}
 
-	arr := strings.Split(s, "")
-	i := 0
-	j := 0
-	m := make(map[string]int, len(arr))
-	for i < len(arr) && j < len(arr) {
-		if _, ok := m[arr[j]]; ok {
-			l := len(m)
-			if l > max {
-				max = l
-			}
-			m = make(map[string]int, len(arr))
-			i++
-			j = i
-		} else {
-			m[arr[j]] = j
-			j++
-			if j == len(arr) {
-				l := len(m)
-				if l > max {
-					max = l
-				}
-				m = make(map[string]int, len(arr))
-				i++
-				j = i
-			}
+		//移动右边届, 条件是移动目标位置的元素不在窗口内也就是不重复
+		for right+1 < len(s) && window[s[right+1]] == 0 {
+			right++
+			window[s[right]] = 1
+		}
+
+		//记录每次窗口遇到重复元素或到底时的长度
+		l := (right - left) + 1
+		if l > max {
+			max = l
 		}
 	}
 	return max
