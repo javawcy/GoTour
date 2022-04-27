c373401577919a516cc7c92b0ac7c2daa77aface
lowdad <javawcy@outlook.com>
2022-04-27 21:48:47 +0800

    fix: leetcode

--- leetcode/longest_palindrome/main.go
+++ leetcode/longest_palindrome/main.go
@@ -5,8 +5,9 @@ import (
 )
 
 func main() {
-	s := "ccc"
-	fmt.Println(longestPalindrome(s))
+	s := "aacabdkacaa"
+	// fmt.Println(longestPalindrome(s))
+	fmt.Println(longestPalindrome1(s))
 }
 
 //中心扩散
@@ -53,5 +54,38 @@ func check(start, end int, arr []byte) []byte {
 
 //动态规划
 func longestPalindrome1(s string) string {
-	return ""
+
+	if len(s) == 0 {
+		return ""
+	}
+	//动态规划备忘录
+	dp := make([][]bool, len(s))
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]bool, len(s))
+	}
+
+	//动态方程
+	// var i, j int
+	// dp[i][j] = (j - i + 1 > 2) && dp[i + 1][j - 1]
+
+	for i := 0; i < len(s); i++ {
+		dp[i][i] = true
+	}
+
+	left, right := 0, 0
+	max := 0
+
+	for j := 1; j < len(s); j++ {
+		for i := 0; i < j; i++ {
+			dp[i][j] = s[i] == s[j] && ((j-1)-(i+1) < 1 || dp[i+1][j-1])
+			if dp[i][j] {
+				if j-i+1 > max {
+					max = j - i + 1
+					left = i
+					right = j
+				}
+			}
+		}
+	}
+	return s[left : right+1]
 }
